86b2224463049c4e73c7567d0113a527e9accccc
firmafadi <firmafirmansyah@gmail.com>
2022-06-17 15:34:33 +0700

    add sidebar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Calendar, ColorPicker, Customers, Ecommerce, Editor, Employees, Kanban, Orders, Area, Bar, ColorMapping, Financial, Line, Pie, Pyramid, Stacked, Sparkline} from './pages';  
+
+import { useStateContext } from './contexts/ContextProvider';
 
 import './App.css'
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
   return (
     <div>
       <BrowserRouter>
@@ -23,11 +27,11 @@ const App = () => {
           </div>
           {activeMenu ? (
             <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
-              Sidebar
+              <Sidebar/>
             </div>
           ): (
             <div className='w-0 dark:bg-secondary-dark-bg'>
-              Sidebar w-0
+               <Sidebar/>
             </div>
           )}
 
@@ -36,33 +40,33 @@ const App = () => {
             ${activeMenu ? 'md:ml-72':'flex-2'} `
           }>
             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
-              Navbar
+              <Navbar/>
             </div>
           </div>
 
           <div>
             <Routes>
               {/* Dashboard */}
-              <Route path='/' element='ECommerce'/>
-              <Route path='/ecommerce' element='ECommerce'/>
+              <Route path='/' element=''/>
+              <Route path='/ecommerce' element={ <Ecommerce/> }/>
               {/* Pages */}
-              <Route path='/orders' element='Orders'/>
-              <Route path='/employees' element='Employees'/>
-              <Route path='/customers' element='Customers'/>
+              <Route path='/orders' element={ <Orders/> }/>
+              <Route path='/employees' element={ <Employees/> }/>
+              <Route path='/customers' element={ <Customers/> }/>
               {/* Apps */}
-              <Route path='/kanban' element='Kanban'/>
-              <Route path='/editor' element='Editor'/>
-              <Route path='/calendar' element='Calendar'/>
-              <Route path='/color-picker' element='ColorPicker'/>
+              <Route path='/kanban' element={ <Kanban/> }/>
+              <Route path='/editor' element={ <Editor/> }/>
+              <Route path='/calendar' element={ <Calendar/> }/>
+              <Route path='/color-picker' element={ <ColorPicker/> }/>
               {/* charts  */}
-              {/* <Route path="/line" element={<Line />} />
+              <Route path="/line" element={<Line />} />
               <Route path="/area" element={<Area />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/financial" element={<Financial />} />
               <Route path="/color-mapping" element={<ColorMapping />} />
               <Route path="/pyramid" element={<Pyramid />} />
-              <Route path="/stacked" element={<Stacked />} /> */}
+              <Route path="/stacked" element={<Stacked />} />
 
             </Routes>
           </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,15 +8,9 @@ import { links } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-  const { currentColor, activeMenu, setActiveMenu, screenSize } = useStateContext();
+  const { activeMenu, setActiveMenu } = useStateContext();
 
-  const handleCloseSideBar = () => {
-    if (activeMenu !== undefined && screenSize <= 900) {
-      setActiveMenu(false);
-    }
-  };
-
-  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white  text-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
 
   return (
@@ -24,14 +18,18 @@ const Sidebar = () => {
       {activeMenu && (
         <>
           <div className="flex justify-between items-center">
-            <Link to="/" onClick={handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
-              <SiShopware /> <span>Shoppy</span>
+            <Link to="/" 
+              onClick={() => setActiveMenu(false)}
+              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
+            >
+              <SiShopware /> <span>Dash</span>
             </Link>
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
                 type="button"
-                onClick={() => setActiveMenu(!activeMenu)}
-                style={{ color: currentColor }}
+                onClick={() => setActiveMenu(
+                  (prevActiveMenu) => !prevActiveMenu
+                )}
                 className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
               >
                 <MdOutlineCancel />
@@ -48,10 +46,6 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={handleCloseSideBar}
-                    style={({ isActive }) => ({
-                      backgroundColor: isActive ? currentColor : '',
-                    })}
                     className={({ isActive }) => (isActive ? activeLink : normalLink)}
                   >
                     {link.icon}
